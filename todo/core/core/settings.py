e8deb240fbda5e14dbe9c68e87c53b795c497992
Manjit-P <manjeetpalli53@gmail.com>
2025-07-26 21:22:39 +0545

    postgres connection

--- todo/core/core/settings.py
+++ todo/core/core/settings.py
@@ -95,15 +95,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todo_db',
-        'USER': 'zenrich',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+    'default': env.dj_db_url("DATABASE_URL")}
 
 
 # Password validation
